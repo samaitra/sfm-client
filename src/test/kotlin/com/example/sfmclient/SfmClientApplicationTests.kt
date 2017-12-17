506ff50ca233ee27c0536ccaf5c9d65001694bb8
samaitra <saikat.maitra@gmail.com>
2017-12-17 00:42:20 -0600

    adding client app and properties

---
+++ src/test/kotlin/com/example/sfmclient/SfmClientApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.sfmclient
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SfmClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
